ab89d65a3256c1f30a41660e0247c94e15d264bb
harika-09 <haarikakota@gmail.com>
2020-03-16 12:01:31 -0500

    object type

---
+++ object/index.js
@@ -0,0 +1,20 @@
+ 
+
+ let person = {
+     name :"Harika",
+     age : 22
+ };
+
+//Dot notation
+person.name ="Kota";
+
+//Bracket notation
+
+person['name'] ="Harika Kota"
+
+//Bracket notation can also be used by selecting specific caegory
+let selection = 'name';
+person[selection] ='Harikakota'
+
+ console.log(person);
+ console.log(person.name);
---
+++ primitivetypes/index.js
@@ -0,0 +1,6 @@
+let name ="Harika";//string literal
+let age = 22;//umber literal
+let isApproved = false;//Boolean literal
+let firstName = undefined;
+let lastName = null;//null is used to set any variable to null
+
