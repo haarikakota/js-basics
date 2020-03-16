992c6b672c4b9d0b4f96509e190bfd12da650609
harika-09 <haarikakota@gmail.com>
2020-03-16 11:41:23 -0500

    constants

---
+++ constants/index.js
@@ -0,0 +1,12 @@
+//let can be reassigned
+
+let interestRate =0.3;
+interestRate = 1;
+console.log(interestRate);
+
+
+//const cannot be reassigned
+
+const interestRate=0.3;
+interestRate = 1;
+console.log(interestRate);
