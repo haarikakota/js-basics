67a37bb8d99071dcd995110c1dd20002575b0288
harika-09 <haarikakota@gmail.com>
2020-03-16 11:14:47 -0500

    variables

---
+++ variables/index.js
@@ -0,0 +1,16 @@
+
+
+let name = "Harika Kota";
+console.log(name);
+
+//(name) cannot be a reserved keywords
+// instead of name x cannot be used (which means it should be meaningful)
+//cannot start with a number (1name)
+//cannot contain a space or hypen(-) 
+//variable names are case sensitive(firstName)
+
+//can declare two variables in a single line but for best practise declare in different line
+
+//this is also possible overe here
+let firstname, lastName;
+
