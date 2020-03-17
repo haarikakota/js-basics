5f164885425e469484ffba0e11af54b3a9ff8a09
harika-09 <haarikakota@gmail.com>
2020-03-17 14:34:52 -0500

    exercises and object basics

---
+++ exercise/demerit.js
@@ -0,0 +1,21 @@
+
+
+checkSpeed(75);
+function checkSpeed(speed){
+    
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+    if(speed <= speedLimit + kmPerPoint)
+    {
+        console.log('ok');
+        return ;
+    }
+    else{
+        const points = Math.floor((speed - speedLimit) / kmPerPoint);
+        if(points >= 12)
+        console.log('license suspended');
+        else
+        console.log('points',points);
+    }
+
+}
---
+++ object/constructorfun.js
@@ -0,0 +1,13 @@
+
+
+    //constructor function
+    function Circle(radius){
+        this.radius = radius;
+        this.draw = function(){
+            console.log('draw');
+        }
+
+    }
+
+    const circle = new Circle(1);
+  
